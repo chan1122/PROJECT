57f48355fc08f9dd60e9e34a8aac4c7aa6f9f5e5
chan1122 <zoro3958@naver.com>
2022-10-25 13:35:19 +0900

    aaa

--- JS/main.js
+++ JS/main.js
@@ -7,12 +7,12 @@ $(() => {
   // 메인 트랙 커버박스 클릭 이벤트 //
   $(".mtrackcoverinng").click(function(){
     $(this).css({
-        height:"300px",
+        height:"800px",
         transition: "all .4s ease-in"
     })// 메인 트랙 커버박스 클릭 css //
     $(".mtrack",this)
     .css({
-        transition:"all 1s ease-out .4s",
+        transition:"all 1s ease-out",
         transform:"translate(-50%, -50%) rotateX(0deg) rotateY(0deg)"
     })
   });// 메인 트랙 커버박스 클릭 이벤트 //

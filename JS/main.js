d14ae7fdff4d24ea8001f0cbf4f2e64837f91eff
chan1122 <zoro3958@naver.com>
2022-10-25 09:31:28 +0900

    aa

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,19 @@
 // 음원사이트 메인 페이지 자바스크립트
 
 // 로딩하기
-window.addEventListener("DOMContentLoaded",()=>{
-    //호출해보기
-    console.log("메인..페이지..로딩...완료");
-
-
-
-
-});
+//제이쿼리 로딩구역
+$(() => {
+  /////////////////////////////////////////////
+  // 메인 트랙 커버박스 클릭 이벤트 //
+  $(".mtrackcoverinng").click(function(){
+    $(this).css({
+        height:"300px",
+        transition: "all .4s ease-in"
+    })// 메인 트랙 커버박스 클릭 css //
+    $(".mtrack",this)
+    .css({
+        transition:"all 1s ease-out .4s",
+        transform:"translate(-50%, -50%) rotateX(0deg) rotateY(0deg)"
+    })
+  });// 메인 트랙 커버박스 클릭 이벤트 //
+}); //제이쿼리 로딩구역/////////////////////////

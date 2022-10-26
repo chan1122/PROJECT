425bddc319acca626573a8e35b178e07c0ef9b82
chan1122 <zoro3958@naver.com>
2022-10-26 16:06:30 +0900

    aa

--- JS/main.js
+++ JS/main.js
@@ -5,13 +5,24 @@
 $(() => {
   /////////////////////////////////////////////
   // 메인 트랙 커버박스 클릭 이벤트 //
-  $(".mtrackcoverinng").click(function(){
-    $(this).animate({
-      height:"800px", 
-    },800).
-    $(".mtrack",this)
-    .animate({
-      Transform: "translate(-50%, -50%) rotateX(0) rotateY(0deg) rotate(0deg)"
-    })
-  });// 메인 트랙 커버박스 클릭 이벤트 //
+  $(".mtrackcoverinng").click(function () {
+    $(this).animate(
+      {
+        height: "800px",
+      },function ani1(){
+
+        $(".mtrack",this).css({
+          transform:"translate(-50%, -50%) rotateX(0deg) rotateY(0deg)",
+          animation: "ttrack1ani paused",
+          animation: "tttrack1 2s ease fowards",
+        },setTimeout,800,ani2)// 애니 메이션 2
+
+        function ani2(){
+          $
+        }
+      })//기존 
+      
+        
+       // 메인 트랙
+  }); // 메인 트랙 커버박스 클릭 이벤트 //
 }); //제이쿼리 로딩구역/////////////////////////

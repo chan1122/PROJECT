85bc86ff705477ce7edf6287bba4db6bbcd0d9bf
chan1122 <zoro3958@naver.com>
2022-10-27 12:16:05 +0900

    ㅁㅁ

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,7 @@
 //제이쿼리 로딩구역
 $(() => {
   /////////////////////////////////////////////
+
   // 메인 트랙 커버박스 클릭 이벤트 //
   $(".mtrackcoverinng").click(function () {
     $(this).animate({
@@ -12,19 +13,31 @@ $(() => {
 
     $(".mtrack", this)
       .css({ animation: "paused" })
-      .hide(200, function () {
+      .hide(10, function () {
+        $(this)
+          .css({
+            transform: " translate(-50%, -50%) rotateX(90deg) rotateY(0deg)",
+            animation: "running",
+          })
+          .delay(100)
+          .show(10, function () {
+            $(this).css({
+              transform:
+                "translate(-50%, -50%) rotateX(0deg) rotateY(0deg) rotate(0)",
+            }); // 등장!
+          });
+        }) // 사라진후 이어서!
 
-        $(".mtrack", this)
-        .css({
-          animate:"running",
-          transform: " translate(-50%, -50%) rotateX(90deg) rotateY(0deg)",
-        }).show(2000).animate({
-          transform: " translate(-50%, -50%) rotateX(90deg) rotateY(0deg)"}
-          ,1000)// 등장!
 
+        .parent().siblings()
+        .animate({height:"0"},800)
+        .find(".mtrack")
+        .attr("style","")
+        // .css({
+        //   transform: " translate(-50%, -50%) rotateX(0deg) rotateY(0deg)",
 
-      }); // 사라진후 이어서!
-
+        // })
+        
     // 메인 트랙
   }); // 메인 트랙 커버박스 클릭 이벤트 //
 }); //제이쿼리 로딩구역/////////////////////////

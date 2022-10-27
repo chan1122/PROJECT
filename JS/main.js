84cefd8ab45767292db14c0fd9e2dd55d0a8be59
chan1122 <zoro3958@naver.com>
2022-10-27 16:10:09 +0900

    업뎃!

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,8 @@ $(() => {
   // 메인 트랙 커버박스 클릭 이벤트 //
   $(".mtrackcoverinng").click(function () {
     $(this).animate({
-      height: "800px",
+      height: "700px",
+      overflow: "hidden",
     });
 
     $(".mtrack", this)
@@ -26,18 +27,42 @@ $(() => {
                 "translate(-50%, -50%) rotateX(0deg) rotateY(0deg) rotate(0)",
             }); // 등장!
           });
-        }) // 사라진후 이어서!
+      }) // 사라진후 이어서!
 
+      .parent()
+      .siblings()
+      .animate({ height: "0" }, 800)
+      .find(".mtrack")
+      .attr("style", "");
+    // .css({
+    //   transform: " translate(-50%, -50%) rotateX(0deg) rotateY(0deg)",
 
-        .parent().siblings()
-        .animate({height:"0"},800)
-        .find(".mtrack")
-        .attr("style","")
-        // .css({
-        //   transform: " translate(-50%, -50%) rotateX(0deg) rotateY(0deg)",
+    // })
 
-        // })
-        
     // 메인 트랙
   }); // 메인 트랙 커버박스 클릭 이벤트 //
+
+  let album = $(".album");
+
+  // 제이쿼리 전용
+  // each((순번,자신)=>{})
+
+  album.each((i1,e1)=>{
+
+    $(e1)
+      .find("span")
+      .each((i2, e2) => {
+        $(e2).css({
+          background: `url(images/trackalbum/track${i1-1}-span${i2+1}.jpg)no-repeat center/100% 100%`,
+        });
+      }); ////// each ///////
+
+  });/////// each ///////
+
+
+  // console.log("순번:",a)
+
+  // $(".mtrack").children().find("span:nth-child(1)").css({
+  //   background:`url(../images/trackalbum/track${tr}-span${sp}.jpg)no-repeat center/100% 100%`
+  // })
 }); //제이쿼리 로딩구역/////////////////////////

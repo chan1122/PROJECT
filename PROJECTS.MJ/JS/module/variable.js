ba5f582dd86f70bd18eca9bd683e2c5330029e32
chan1122 <zoro3958@naver.com>
2022-11-30 21:11:18 +0900

    aa

---
+++ PROJECTS.MJ/JS/module/variable.js
@@ -0,0 +1,21 @@
+// 변수 집합소!!
+
+let tfrxry1 = " translate(-50%, -50%) rotateX(90deg) rotateY(0deg)";
+let tfrxry2 = "translate(-50%, -50%) rotateX(0deg) rotateY(0deg) rotate(0)";
+
+let main_album_track_FOR50 = `<div class="mtt1 track mtrackcoverinng">
+    <!-- 오디오 들어가는 자리 JS에서 뿌려주는중!.../../ -->
+    <audio src="#" class="AUDIO" muted></audio>
+    <!-- 앨범---------------3D요소---------------------------- -->
+    <div class="ttrack album mtrack">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </div>`;
+const hb = $("html,boby");
+
+export { tfrxry1, tfrxry2, main_album_track_FOR50, hb };

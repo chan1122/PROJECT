122a424f9667cfe04035934051a437ef90319d91
Min Jeong-Kim <zoro3958@naver.com>
2022-11-30 14:02:02 +0900

    aa

--- PROJECTS.MJ/02.React/react-app-PJ/my-react-app/src/index.js
+++ PROJECTS.MJ/02.React/react-app-PJ/my-react-app/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js는 상단 public/index.html에 적용되는 컴포넌트다.
+// 리액트 돔에 클리이언트를 실행해주는 코드
+import ReactDOM  from "react-dom/client";
+// 상단 
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

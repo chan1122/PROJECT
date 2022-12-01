452159664fd039697b6c55533cfc8dba00470194
chan1122 <zoro3958@naver.com>
2022-12-01 09:37:46 +0900

    aa

--- PROJECTS.MJ/JS/module/variable.js
+++ PROJECTS.MJ/JS/module/variable.js
@@ -1,12 +1,14 @@
-// 변수 집합소!!
+import albnum from "./DBconnect.js";
+let GAG = JSON.parse(JSON.stringify(PMJjsonDATA));
 
+// JQB밖 변수!
 let tfrxry1 = " translate(-50%, -50%) rotateX(90deg) rotateY(0deg)";
+// 세워지는
 let tfrxry2 = "translate(-50%, -50%) rotateX(0deg) rotateY(0deg) rotate(0)";
 
-let main_album_track_FOR50 = `<div class="mtt1 track mtrackcoverinng">
-    <!-- 오디오 들어가는 자리 JS에서 뿌려주는중!.../../ -->
+let main_album_track_FOR50 = `
+<div class="mtt1 track mtrackcoverinng">
     <audio src="#" class="AUDIO" muted></audio>
-    <!-- 앨범---------------3D요소---------------------------- -->
     <div class="ttrack album mtrack">
       <span></span>
       <span></span>
@@ -17,5 +19,37 @@ let main_album_track_FOR50 = `<div class="mtt1 track mtrackcoverinng">
     </div>
     </div>`;
 const hb = $("html,boby");
+const MTC = $(".maintrackcover");
+let ssm = $(".ssm");
+let moff = $(".muteoff");
+let mon = $(".muteon");
 
-export { tfrxry1, tfrxry2, main_album_track_FOR50, hb };
+const mtb = $(".mutedbtn");
+const sm = $(".sm");
+const slidegnb = $(".slidegnb");
+const topA = $(".tbox1");
+const Cl = console.log;
+/*********************************** 
+  [JQB안쪽 변수] 
+***********************************/
+
+
+let lpnum = 0;
+// 추가된변수
+
+export {
+  tfrxry1,
+  tfrxry2,
+  main_album_track_FOR50,
+  hb,
+  MTC,
+  ssm,
+  lpnum,
+  moff,
+  mon,
+  mtb,
+  sm,
+  slidegnb,
+  topA,
+  Cl,
+};
--- PROJECTS.MJ/JS/wheel.js
+++ PROJECTS.MJ/JS/wheel.js
@@ -1,26 +1,22 @@
+import { sm, slidegnb, topA, Cl } from "./module/variable.js";
 // 마우스 휠 (스크롤이벤트 전용 페이지)
 $(() => {
   let scTop;
   let lastSc;
-  const slidegnb = $(".slidegnb");
-  const sm = $(".sm");
-  const topA = $(".tbox1");
-
-
 
   $(window).scroll(function () {
     //  스크롤 위치값 찍기
     scTop = $(this).scrollTop(); // -> 세로 스크롤 위치값을 리턴하는 메서드
     // 콘솔창에 위치값 찍어보기
-    // console.log(scTop)
+    Cl(scTop);
 
-    // 1 . 슬림메뉴 클래스 온 적용
+    // 1 . 상단 큰 로고 클래스 온 적용 + 줄어들기
     if (scTop >= 100) {
-
       topA.css({ position: "fixed" });
       topA.find(".top").slideUp();
-      topA.addClass("on")
-
+      topA.find(".lm").slideUp();
+      topA.addClass("on");
+      // 2. 상단 얇은줄 줄어들고 늘어나기
       if (scTop > lastSc) {
         slidegnb.addClass("up");
         sm.fadeOut();
@@ -31,18 +27,21 @@ $(() => {
     } else {
       topA.css({ position: "relative" });
       topA.find(".top").slideDown();
-      
+      topA.find(".lm").slideDown();
     }
-
+    let aa = $(".main3").offsetTop()
+    Cl(aa);
+    // 스테이지 맞추기
+    // hb.animate(
+    //   {
+    //     scrollTop: $(this).offset().top + "px",
+    //   },
+    //   600
+    // );
     ////////////////////////////
     // 스크롤 방향 알아내기
-    if (scTop > lastSc) {
-      console.log("아래");
-    } else {
-      console.log("윗방향!");
-    }
-    // 마지막 위치 없데이트 필수!
 
+    // 마지막 위치 없데이트 필수!
     lastSc = scTop;
   }); /// 스크롤 ///
 }); ///////  JQB  //////////

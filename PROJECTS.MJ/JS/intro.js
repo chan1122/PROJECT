42626fbf81c3ba7d1365cd69e0395015e9117375
chan1122 <zoro3958@naver.com>
2022-12-18 15:40:24 +0900

    aa

--- PROJECTS.MJ/JS/intro.js
+++ PROJECTS.MJ/JS/intro.js
@@ -1,12 +1,17 @@
-// 음원사이트 인트로페이지 자바스크립트
 // 변수 구역
+
 const page = $(".page");
 const pr = $(".pillar");
-$(() => {
-  console.log("로딩완료!");
 
-  // pgae.each문으로 돌리기
-  $(page.get().reverse()).each((idx, ele) => {
-    $(e.curr).css({ zIndex: $(idx) });
-  });
+console.log("로딩완료!");
+
+// pgae.each문으로 보이는 순서 정하기
+// $(page.get().reverse()).each(function (idx, ele) {
+//   $(this).css({ zIndex: idx + 1 });
+// });
+
+// page클릭시 넘어가기
+page.click(function () {
+  $(this).css({ transform: "rotateY(180deg)", zIndex: 1 });
+  $(this).siblings().css({ zIndex: 0 });
 });

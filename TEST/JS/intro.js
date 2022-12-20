e65ebfaeb323d3f0da779ca00db0dbff6eeb6e90
chan1122 <zoro3958@naver.com>
2022-12-20 19:38:09 +0900

    aa

--- TEST/JS/intro.js
+++ TEST/JS/intro.js
@@ -1,7 +1,29 @@
 const cc = $(".cube_cover");
-const ct = $(".cube_templet")
+const ct = $(".cube_templet");
+const wp = $(".wrapp");
+const aT = $(".ani_turn");
+let forisappend = `<div class="cube_templet"></div>`;
+
 function cube_templet_for() {
-  for (i = 0; i < 6; i++) {
-    cc.
+  for (let i = 0; i < 6; i++) {
+    cc.append(forisappend);
   }
 }
+
+cube_templet_for();
+
+wp.click(function () {
+  $(this).toggleClass("on");
+  aT.toggleClass("on");
+});
+
+$("html body").on("mousewheel wheel", function (e) {
+  let i;
+  e = window.event || e;
+  let delta = e.wheelDelta || e.detail;
+
+  if (delta > 0 && aT.filter(".on")) {
+    i++;
+    console.log(i);
+  } else if (delta < 0 && aT.filter(".on"));
+});
